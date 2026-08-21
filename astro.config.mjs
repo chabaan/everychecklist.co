efd5a08ec32182a959b0e52165fd5801e292091f
chabaan <98769257+chabaan@users.noreply.github.com>
2026-08-22 00:32:19 +0100

    Update: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://everychecklist.co',
-  outDir: './dist',
+	site: 'https://everychecklist.co',
+	integrations: [sitemap()],
+	// No "base" needed since we're using a custom domain (not a subpath)
 });
